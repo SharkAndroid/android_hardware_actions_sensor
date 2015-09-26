e21e0292fcf53928af754c13f7a75af370c2e0dc
iBullRay <bullray097@yandex.ru>
2015-09-26 20:31:24 +0300

    Android.mk: Fixed error with libs
    Signed-off-by: iBullRay <bullray097@yandex.ru>

--- Android.mk
+++ Android.mk
@@ -28,13 +28,9 @@ LOCAL_SRC_FILES := \
 	TemperatureSensor.cpp \
     InputEventReader.cpp
 
-LOCAL_SHARED_LIBRARIES := \
-	liblog \
-	libcutils \
-	libdl
+LOCAL_SHARED_LIBRARIES := liblog libcutils libdl
 
 LOCAL_PRELINK_MODULE := false
-
 LOCAL_MODULE := sensors.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_MODULE_TAGS := optional
