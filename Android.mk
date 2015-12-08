7d0121976fd8623a5e244bfe56a4fee7efecffde
iBullRay <bullray097@yandex.ru>
2015-12-08 14:43:26 +0200

    sensor: Fixed error with syntax
    Signed-off-by: iBullRay <bullray097@yandex.ru>

--- Android.mk
+++ Android.mk
@@ -14,7 +14,7 @@
 # limitations under the License.
 
 # Sensor driver for ATM702X
-ifeq ($(BOARD_HAVE_ACTIONS_SENSOR), true)
+ifeq ($(strip $(BOARD_HAVE_ACTIONS_SENSOR)), true)
 
 LOCAL_PATH := $(call my-dir)
 
